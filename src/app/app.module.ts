fd6039532f947670cfbcfd7a88e2208ab1a83066
Tomas Zagorskis <tomzagos@gmail.com>
2023-03-07 20:38:52 +0200

    implement reactive form with validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ClientDetailFormComponent } from './components/client-detail-form/client-detail-form.component';
+import { InvoiceComponent } from './components/invoice/invoice.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, ClientDetailFormComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ClientDetailFormComponent, InvoiceComponent],
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/client-detail-form/client-detail-form.component.ts
+++ src/app/components/client-detail-form/client-detail-form.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-client-detail-form',
   templateUrl: './client-detail-form.component.html',
-  styleUrls: ['./client-detail-form.component.scss']
+  styleUrls: ['./client-detail-form.component.scss'],
 })
-export class ClientDetailFormComponent {
+export class ClientDetailFormComponent implements OnInit {
+  clientForm!: FormGroup;
+  countries: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<{ [key: string]: { name: string } }>(
+        'https://restcountries.com/v2/all'
+      )
+      .pipe(
+        map((responseData) => {
+          for (const key in responseData) {
+            if (Object.prototype.hasOwnProperty.call(responseData, key)) {
+              const element = responseData[key];
+              this.countries.push(element.name);
+            }
+          }
+        })
+      )
+      .subscribe();
+    console.log(this.countries);
+
+    this.clientForm = new FormGroup({
+      isVATPayer: new FormControl('private'),
+      fName: new FormControl(null, Validators.required),
+      lName: new FormControl(null, Validators.required),
+      company: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      country: new FormControl(null, Validators.required),
+      city: new FormControl(null, Validators.required),
+      address: new FormControl(null, Validators.required),
+    });
+  }
+  onSubmit() {
+    console.log(this.clientForm.value);
+  }
 }
