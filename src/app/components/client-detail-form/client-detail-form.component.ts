267608d0d1b055429b15f4f415768c648d85cc7e
Tomas Zagorskis <tomzagos@gmail.com>
2023-03-14 10:27:47 +0200

    fix style, validation

--- src/app/components/client-detail-form/client-detail-form.component.ts
+++ src/app/components/client-detail-form/client-detail-form.component.ts
@@ -9,7 +9,7 @@ import { map } from 'rxjs';
   styleUrls: ['./client-detail-form.component.scss'],
 })
 export class ClientDetailFormComponent implements OnInit {
-  clientForm!: FormGroup;
+  invoiceForm!: FormGroup;
   countries: any[] = [];
 
   constructor(private http: HttpClient) {}
@@ -30,20 +30,40 @@ export class ClientDetailFormComponent implements OnInit {
         })
       )
       .subscribe();
-    console.log(this.countries);
 
-    this.clientForm = new FormGroup({
-      isVATPayer: new FormControl('private'),
-      fName: new FormControl(null, Validators.required),
-      lName: new FormControl(null, Validators.required),
-      company: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      country: new FormControl(null, Validators.required),
-      city: new FormControl(null, Validators.required),
-      address: new FormControl(null, Validators.required),
+    this.invoiceForm = new FormGroup({
+      customerDetails: new FormGroup({
+        VATPayer: new FormControl('private'),
+        fName: new FormControl(null, Validators.required),
+        lName: new FormControl(null, Validators.required),
+        company: new FormControl(null),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        country: new FormControl(null, Validators.required),
+        city: new FormControl(null, Validators.required),
+        address: new FormControl(null, Validators.required),
+      }),
+      serviceProvider: new FormGroup({
+        name: new FormControl(null, Validators.required),
+        sCountry: new FormControl(null, Validators.required),
+        sCity: new FormControl(null, Validators.required),
+        sAddress: new FormControl(null, Validators.required),
+      }),
     });
+
+    // Checks customer for type and adds/removes validation for company name input
+    this.invoiceForm
+      .get('customerDetails.VATPayer')
+      ?.valueChanges.subscribe((val: string) => {
+        const company = this.invoiceForm.get('customerDetails.company');
+        val === 'business'
+          ? company?.addValidators(Validators.required)
+          : company?.clearValidators();
+        if (val === 'private') company?.setValue(null);
+        company?.updateValueAndValidity();
+      });
   }
+
   onSubmit() {
-    console.log(this.clientForm.value);
+    console.log(this.invoiceForm.value);
   }
 }
